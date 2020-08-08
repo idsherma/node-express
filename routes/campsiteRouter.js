08ba0b47bc92c27ab89cf8e0e469593f3fb4b253
idsherma <idsherma@gmail.com>
2020-08-07 22:18:43 -0400

    Express Router

---
+++ routes/campsiteRouter.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+//creating a new router 
+const campsiteRouter = express.Router();
+
+campsiteRouter.use(bodyParser.json());
+
+/* 
+converted this code to the code below
+
+app.all('/campsites', (req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+});
+
+app.get('/campsites', (req, res) => {
+    res.end('Will send all the campsites to you');
+});
+
+app.post('/campsites', (req, res) => {
+    res.end(`Will add the campsite: ${req.body.name} with description: ${req.body.description}`);
+});
+
+app.put('/campsites', (req, res) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /campsites');
+});
+
+app.delete('/campsites', (req, res) => {
+    res.end('Deleting all campsites');
+});
+*/
+
+campsiteRouter.route('/')
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req, res) => {
+    res.end('Will send all the campsites to you');
+})
+.post((req, res) => {
+    res.end(`Will add the campsite: ${req.body.name} with description: ${req.body.description}`);
+})
+.put((req, res) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /campsites');
+})
+.delete((req, res) => {
+    res.end('Deleting all campsites');
+});
+
+module.exports = campsiteRouter;
